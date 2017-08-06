e5eab6ff0f31c51a6a7a3bd5efe82ddade926530
jeremylenz <jeremylenz@gmail.com>
2017-08-06 14:32:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+
+let h1 = React.createElement('h1', {}, "An Awesome Person")
+let p = React.createElement('p', {}, "Who is learning React")
+let listItems = []
+let list = ["JavaScript", "React", "Movies", "Ice cream"]
+list.forEach((interest) => {
+  listItems.push(React.createElement('li', {}, interest))
+})
+let ul = React.createElement('ul', {className: 'my-interests'}, listItems)
+let meInReact = React.createElement('div', {className: 'me'}, [h1, p, ul])
+
+
+
 ReactDOM.render(
-  meInReact, 
+  meInReact,
   document.getElementById('root')
 );
 
 export default meInReact
-*/
